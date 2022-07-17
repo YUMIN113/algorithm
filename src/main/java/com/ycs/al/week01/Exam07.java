6b3c7ca075a50acde19640306bf84be19f970ee4
YUMIN113 <cfy113@outlook.com>
2022-07-17 20:39:13 +0900

    A+B

---
+++ src/main/java/com/ycs/al/week01/Exam07.java
@@ -0,0 +1,22 @@
+
+// - 문제
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+// - 입력
+// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+// -출력
+// 첫째 줄에 A+B를 출력한다.
+
+package com.ycs.al.week01;
+
+public class Exam07 {
+	public static void main(String[] args) {
+    
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+
+		System.out.println(A+B);
+	}
+}
