27369360dec858a9d5ec2e4eef23af0472f830ba
YUMIN113 <cfy113@outlook.com>
2022-07-17 20:22:11 +0900

    개

---
+++ src/main/java/com/ycs/al/week01/Exam06.java
@@ -0,0 +1,19 @@
+
+// - 문제
+// 개를 출력하시오.
+
+package com.ycs.al.week01;
+
+public class Exam06 {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
+
+// Escape Sequence
+// \" : Insert a double quote character in the text at this point.
+// \\ : Insert a backslash character in the text at this point.
