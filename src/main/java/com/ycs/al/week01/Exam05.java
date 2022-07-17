5b73eb061f5119972eea31f55adad2c92c24ca37
YUMIN113 <cfy113@outlook.com>
2022-07-17 20:21:27 +0900

    고양이

--- src/main/java/com/ycs/al/week01/Exam05.java
+++ src/main/java/com/ycs/al/week01/Exam05.java
@@ -1,3 +1,5 @@
+
+// - 문제
 // 고양이를 출력하시오.
 
 package com.ycs.al.week01;
@@ -10,11 +12,8 @@ public class Exam05 {
 		System.out.println("(  /  )");
 		System.out.println(" \\(__)|");
 
-		// Escape Sequence
-		// '\' 는 문자 그 자체만으로는 출력되지 않는 특수문자이다.
-		// '\n' 등으로 사용.
-		// 따라서 '\' 출력 원한다면 '\\' 로 입력해야 한다.
-
 	}
-	
 }
+
+// Escape Sequence
+// \\ : Insert a backslash character in the text at this point.
