98a1e9bf90f0a0b692c649bd389e239fd2009cd2
yumin <cfy113@naver.com>
2022-11-28 20:59:05 +0900

    백준_10699번_오늘 날짜

---
+++ algorithm/src/algorithm/step04/A10699.java
@@ -0,0 +1,32 @@
+// 백준
+// 10699
+
+// 문제)
+// 서울의 오늘 날짜를 출력하는 프로그램을 작성하시오.
+
+// 입력)
+// 입력은 없다.
+
+// 출력)
+// 서울의 오늘 날짜를 "YYYY-MM-DD" 형식으로 출력한다.
+
+
+package algorithm.step04;
+
+import java.time.LocalDate;
+
+public class A10699 {
+	
+	public static void main(String[] args) {
+	
+	  LocalDate localDate = LocalDate.now();
+	
+	  System.out.println(localDate);
+	
+	}
+
+}
+
+
+// LocalDate Class : 년, 월, 일 정보만 가짐
+// LocalDate.now() : 오늘 년, 월, 일 정보 나타
