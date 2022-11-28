e042293d81076c730eeb330d385bc54fc95d0218
yumin <cfy113@naver.com>
2022-11-28 21:54:03 +0900

    백준_25083_새싹

---
+++ algorithm/src/algorithm/step04/A25083.java
@@ -0,0 +1,28 @@
+// 백준
+// 25083
+
+// 문제)
+// 아래 예제와 같이 새싹을 출력하시오.
+
+// 입력)
+// 입력은 없다.
+
+// 출력)
+// 새싹을 출력한다.
+
+
+package algorithm.step04;
+
+public class A25083 {
+	
+	public static void main(String[] args) {
+		
+	
+	System.out.println("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+	
+	}
+}
+
+
+// Escape Sequence
+// \' , \" , \\ : ' , " , \ 그대로 출력하기 위해서 backslash를 덧붙인다.
