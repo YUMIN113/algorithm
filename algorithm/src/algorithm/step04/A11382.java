dda2e55670ef031686016f192436e6670207f41f
yumin <cfy113@naver.com>
2022-11-28 22:21:04 +0900

    백준_11382_꼬마 정민

---
+++ algorithm/src/algorithm/step04/A11382.java
@@ -0,0 +1,42 @@
+// 백준
+// 11382
+
+// 문제)
+// 꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+
+// 입력)
+// 첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 10의 12승)이 공백을 사이에 두고 주어진다.
+
+// 출력)
+// A+B+C의 값을 출력한다.
+
+// 예제 입력)
+// 77 77 7777
+
+// 예제 출력)
+// 7931
+
+
+package algorithm.step04;
+
+import java.util.Scanner;
+
+public class A11382 {
+	
+	public static void main(String[] args) {
+		
+	  Scanner sc = new Scanner(System.in);
+	  
+	  long numA = sc.nextLong();
+	  long numB = sc.nextLong();
+	  long numC = sc.nextLong();
+	  
+	  System.out.println(numA + numB + numC);
+	  
+	  sc.close();
+	  		
+	}
+
+}
+
+// 숫자 입력 범위를 확인하자.
